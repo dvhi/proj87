f6440315e1a549b88d380c9141aa03308de9be1c
dvhi <131928785+dvhi@users.noreply.github.com>
2023-04-26 17:38:12 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokestyle = "grey";
+ctx.linewidth = 1;
+ctx.rect() ("x 150", "y 140", "width 430", "height 143");
+ctx.stroke();
+
+ctx.beginPath;
+ctx.strokestyle = "blue";
+ctx.linewidth = 5;
+ctx.arc()(x = 250, y = 210, r = 40, startAngle = 0, endAngle = 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath;
+ctx.strokestyle = "black";
+ctx.linewidth = 5;
+ctx.arc()(x = 350, y = 210, r = 40, startAngle = 0, endAngle = 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath;
+ctx.strokestyle = "red";
+ctx.linewidth = 5;
+ctx.arc()(x = 210, y = 40, r = 40, startAngle = 0, endAngle = 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath;
+ctx.strokestyle = "orange";
+ctx.linewidth = 5;
+ctx.arc()(x = 300, y = 250, r = 40, startAngle = 0, endAngle = 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath;
+ctx.strokestyle = "green";
+ctx.linewidth = 5;
+ctx.arc()(x = 400, y = 250, r = 40, startAngle = 0, endAngle = 2 * Math.PI);
+ctx.stroke();
